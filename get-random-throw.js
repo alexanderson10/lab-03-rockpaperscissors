502df3bda44e7b8a49678191d48510345a7fca40
Vance-M <myveryvills@yahoo.com>
2021-01-14 16:21:47 -0800

    added win/lose tracker added tests

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,7 +1,6 @@
 export function getRandomThrow() {
-    let randomNumber = Math.random()
-    if (randomNumber <= .33) return "rock"
-    if (randomNumber > .333 && randomNumber <= .66) return "paper"
-    else return "scissor"
-
+    let randomNumber = Math.random();
+    if (randomNumber <= .33) return 'rock';
+    if (randomNumber > .333 && randomNumber <= .66) return 'paper';
+    else return 'scissor';
 }
